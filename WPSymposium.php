376aacd5e5c7dc613a4d89277d908fc97ed45ac1
Stephen Edgar <stephen@netweb.com.au>
2014-05-15 13:12:35 +1000

    WPSymposium - 1st pass update
    * Cleans up PHPDoc for WPSymposium Converter
    * Fixes Forum slug field mapping
    * Add Topics section
    * Adds Reply Section
    * Adds callback_topic_status for open or closed topics
    * Adds callback_sticky_status for sticky topics
    * Adds reserved callback callback_forum_type to be implemented
    * Adds reserved callback callback_forum_status to be implemented

--- WPSymposium.php
+++ WPSymposium.php
@@ -3,8 +3,8 @@
 /**
  * Implementation of WP Symposium converter
  *
- * @since bbPress (r4689)
- * @link Codex Docs http://codex.bbpress.org/import-forums/custom-import
+ * @since bbPress (rXXXX)
+ * @link Codex Docs http://codex.bbpress.org/import-forums/wp-symposium
  */
 class WPSymposium extends BBP_Converter_Base {
 
@@ -24,8 +24,6 @@ class WPSymposium extends BBP_Converter_Base {
 	public function setup_globals() {
 
 		/** Forum Section *****************************************************/
-
-		// Setup table joins for the forum section at the base of this section
 
 		// Forum id (Stored in postmeta)
 		$this->field_map[] = array(
@@ -54,7 +52,7 @@ class WPSymposium extends BBP_Converter_Base {
 		// Forum slug (Clean name to avoid confilcts)
 		$this->field_map[] = array(
 			'from_tablename'  => 'symposium_cats',
-			'from_fieldname'  => 'slug',
+			'from_fieldname'  => 'stub',
 			'to_type'         => 'forum',
 			'to_fieldname'    => 'post_name',
 		);
@@ -96,6 +94,233 @@ class WPSymposium extends BBP_Converter_Base {
 			'to_fieldname'    => 'post_modified_gmt',
 			'default' => date('Y-m-d H:i:s')
 		);
+
+		/** Topic Section *****************************************************/
+
+		// Setup table joins for the topic section at the base of this section
+
+		// Topic id (Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'tid',
+			'from_expression' => 'WHERE symposium_topics.topic_parent = 0',
+			'to_type'         => 'topic',
+			'to_fieldname'    => '_bbp_topic_id'
+		);
+
+		// Topic parent forum id (If no parent, then 0. Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_category',
+			'to_type'         => 'topic',
+			'to_fieldname'    => '_bbp_forum_id',
+			'callback_method' => 'callback_forumid'
+		);
+
+		// Topic author.
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_owner',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_author',
+			'callback_method' => 'callback_userid'
+		);
+
+		/* This looks like IPv6, we don't support IPv6 in the importers yet
+		// Topic author ip (Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'remote_addr',
+			'to_type'         => 'topic',
+			'to_fieldname'    => '_bbp_author_ip'
+		);
+		*/
+
+		// Topic content.
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_post',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_content',
+			'callback_method' => 'callback_html'
+		);
+
+		// Topic title.
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_subject',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_title'
+		);
+
+		// Topic slug (Clean name to avoid conflicts)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'stub',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_name',
+			'callback_method' => 'callback_slug'
+		);
+
+		// Topic status (Open or Closed)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'allow_replies',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_status',
+			'callback_method' => 'callback_topic_status'
+		);
+
+		// Topic parent forum id (If no parent, then 0)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_category',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_parent',
+			'callback_method' => 'callback_forumid'
+		);
+
+		// Sticky status (Stored in postmeta))
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_sticky',
+			'to_type'         => 'topic',
+			'to_fieldname'    => '_bbp_old_sticky_status',
+			'callback_method' => 'callback_sticky_status'
+		);
+
+		// Topic dates.
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_started',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_date',
+			'callback_method' => 'callback_datetime'
+		);
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_started',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_date_gmt',
+			'callback_method' => 'callback_datetime'
+		);
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_date',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_modified',
+			'callback_method' => 'callback_datetime'
+		);
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_date',
+			'to_type'         => 'topic',
+			'to_fieldname'    => 'post_modified_gmt',
+			'callback_method' => 'callback_datetime'
+		);
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_date',
+			'to_type'         => 'topic',
+			'to_fieldname'    => '_bbp_last_active_time',
+			'callback_method' => 'callback_datetime'
+		);
+
+		/** Reply Section *****************************************************/
+
+		// Reply id (Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'tid',
+			'from_expression' => 'WHERE symposium_topics.topic_parent != 0',
+			'to_type'         => 'reply',
+			'to_fieldname'    => '_bbp_post_id'
+		);
+
+		// Reply parent forum id (If no parent, then 0. Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_category',
+			'to_type'         => 'reply',
+			'to_fieldname'    => '_bbp_forum_id',
+			'callback_method' => 'callback_topicid_to_forumid'
+		);
+
+		// Reply parent topic id (If no parent, then 0. Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_parent',
+			'to_type'         => 'reply',
+			'to_fieldname'    => '_bbp_topic_id',
+			'callback_method' => 'callback_topicid'
+		);
+
+		/* This looks like IPv6, we don't support IPv6 in the importers yet
+		// Reply author ip (Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'remote_addr',
+			'to_type'         => 'reply',
+			'to_fieldname'    => '_bbp_author_ip'
+		);
+		*/
+
+		// Reply author.
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_owner',
+			'to_type'         => 'reply',
+			'to_fieldname'    => 'post_author',
+			'callback_method' => 'callback_userid'
+		);
+
+		// Reply content.
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_post',
+			'to_type'         => 'reply',
+			'to_fieldname'    => 'post_content',
+			'callback_method' => 'callback_html'
+		);
+
+		// Reply parent topic id (If no parent, then 0)
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_parent',
+			'to_type'         => 'reply',
+			'to_fieldname'    => 'post_parent',
+			'callback_method' => 'callback_topicid'
+		);
+
+		// Reply dates.
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_started',
+			'to_type'         => 'reply',
+			'to_fieldname'    => 'post_date',
+			'callback_method' => 'callback_datetime'
+		);
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_started',
+			'to_type'         => 'reply',
+			'to_fieldname'    => 'post_date_gmt',
+			'callback_method' => 'callback_datetime'
+		);
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_started',
+			'to_type'         => 'reply',
+			'to_fieldname'    => 'post_modified',
+			'callback_method' => 'callback_datetime'
+		);
+		$this->field_map[] = array(
+			'from_tablename'  => 'symposium_topics',
+			'from_fieldname'  => 'topic_started',
+			'to_type'         => 'reply',
+			'to_fieldname'    => 'post_modified_gmt',
+			'callback_method' => 'callback_datetime'
+		);
 	}
 
 	/**
@@ -123,5 +348,89 @@ class WPSymposium extends BBP_Converter_Base {
 	public function authenticate_pass( $password, $serialized_pass ) {
 		$pass_array = unserialize( $serialized_pass );
 		return ( $pass_array['hash'] == md5( md5( $password ). $pass_array['salt'] ) );
+	}
+
+	/**
+	 * Translate the forum type from WP Symposium numeric's to WordPress's strings.
+	 *
+	 * @param int $status WP Symposium numeric forum type
+	 * @return string WordPress safe
+	 *
+	 * This can be included when more info on how WP Symposium handles forum types
+	public function callback_forum_type( $status = 1 ) {
+		switch ( $status ) {
+			case 0 :
+				$status = 'category';
+				break;
+
+			case 1  :
+			default :
+				$status = 'forum';
+				break;
+		}
+		return $status;
+	}
+	*/
+
+	/**
+	 * Translate the forum status from WP Symposium numeric's to WordPress's strings.
+	 *
+	 * @param int $status WP Symposium numeric forum status
+	 * @return string WordPress safe
+	 *
+	 * This can be included when more info on how WP Symposium handles forum status
+	public function callback_forum_status( $status = 0 ) {
+		switch ( $status ) {
+			case 1 :
+				$status = 'closed';
+				break;
+
+			case 0  :
+			default :
+				$status = 'open';
+				break;
+		}
+		return $status;
+	}
+	*/
+
+	/**
+	 * Translate the topic status from WP Symposium strings to WordPress's strings.
+	 *
+	 * @param $status WP Symposium string topic status
+	 * @return string WordPress safe
+	 */
+	public function callback_topic_status( $status = 0 ) {
+		switch ( $status ) {
+			case '' :
+				$status = 'closed';       // WP Symposium closed topic "allow_replies = ''"
+				break;
+
+			case 'on'  :
+			default :
+				$status = 'publish';       // WP Symposium Normal Topic "allow_replies = 'on'"
+				break;
+		}
+		return $status;
+	}
+
+	/**
+	 * Translate the topic sticky status type from WP Symposium numeric's to WordPress's strings.
+	 *
+	 * @param int $status WP Symposium numeric forum type
+	 * @return string WordPress safe
+	 */
+	public function callback_sticky_status( $status = 0 ) {
+		switch ( $status ) {
+			case 1 :
+				$status = 'sticky';       // WP Symposium Sticky topic 'topic_sticky = 1'
+				break;
+
+			case 0  :
+			default :
+				$status = 'normal';       // WP Symposium normal topic 'topic_sticky = 0'
+				break;
+		}
+		return $status;
 	}
 }
